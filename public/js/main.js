89deb957614994d1eaf49de0de4b4416e8aa3d48
Kevin <dqtkien@gmail.com>
2019-11-29 22:44:54 +0700

    Add Laravel Framework

---
+++ public/js/main.js
@@ -0,0 +1,62 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Navigation Button : Search, Login, Shopcart
+    let btn = [];
+    icon = document.getElementsByClassName("nav__icon");
+    btn = document.getElementsByClassName("form__text");
+    let count = [];
+    let icon_length = icon.length;
+    for (let i = 0; i < icon_length; i++) {
+        count[i] = 0;
+    }
+    for (let index = 0; index < icon_length; index++) {
+        icon[index].addEventListener("click", function () {
+
+            if (btn[index].classList.contains("form__text--appear")) {
+                btn[index].classList.remove("form__text--appear");
+                for (let i of btn) {
+                    i.classList.remove("form__text--appear");
+                }
+            }
+            else {
+                for (let i of btn) {
+                    i.classList.remove("form__text--appear");
+                }
+                btn[index].classList.toggle('form__text--appear')
+            }
+        });
+    }
+
+    let navbar;
+    navbar = document.querySelector(".navbar-nav");
+    navbar.addEventListener("click", function () {
+        for (let item of btn) {
+            item.classList.remove("form__text--appear");
+        }
+    })
+    // End Navigation Button : Search, Login, Shopcart
+    // Show Signup Popup
+    let popup = document.querySelector('.popup');
+    let signupbtn = document.querySelector('.nav__signup--link');
+    let blackscreen = document.querySelector('.blackscreen');
+    signupbtn.addEventListener('click', function(){
+        popup.classList.add('appeared');
+        btn[1].classList.remove('form__text--appear');
+    })
+    blackscreen.addEventListener('click', function(){
+        popup.classList.remove('appeared');
+    })
+
+    // Click x to close popup
+    let closeup = document.querySelector('.closeup');
+    closeup.addEventListener('click', function(){
+        popup.classList.remove('appeared');
+    })
+    let home_carousel;
+    home_carousel = document.querySelector(".home__carousel");
+    home_carousel.addEventListener("click", function () {
+        for (let item of btn) {
+            item.classList.remove("form__text--appear");
+        }
+    })
+
+})
