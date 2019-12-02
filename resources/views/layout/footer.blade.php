4b5e0433fa8ab7c86603269da573bcd47096d0c6
Kevin <dqtkien@gmail.com>
2019-12-02 11:20:27 +0700

    Add header + footer template

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,90 @@
+    <!-- Footer -->
+    <footer>
+        <hr>
+        <div class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-to">
+                        <div class="footer__title">
+                            hỗ trợ khách hàng
+                        </div>
+                        <div class="footer__text">
+                            <div class="footer__emphasis">
+                                Hotline đặt hàng: 1800-6963 <br>
+                            </div>
+                            (Miễn phí , 8-21h kể cả T7, CN) <br>
+                        </div>
+                        <div class="footer__text">
+                            <div class="footer__emphasis">
+                                Hotline chăm sóc khách hàng: 1900-6035 <br>
+                            </div>
+                            (1000đ/phút , 8-21h kể cả T7, CN)
+                        </div>
+                        <a href="#" class="footer__text">Các câu hỏi thưởng gặp</a>
+                        <a href="#" class="footer__text">Các câu hỏi thưởng gặp</a>
+                        <a href="#" class="footer__text">Các câu hỏi thưởng gặp</a>
+                        <a href="#" class="footer__text">Các câu hỏi thưởng gặp</a>
+                        <a href="#" class="footer__text">Các câu hỏi thưởng gặp</a>
+                        <a href="#" class="footer__text">Các câu hỏi thưởng gặp</a>
+                    </div>
+                    <div class="col-nho">
+                        <div class="footer__title">
+                            Về chúng tôi
+                        </div>
+                        <a href="#" class="footer__text">
+                            Giới thiệu
+                        </a>
+                        <a href="#" class="footer__text">
+                            Chính sách bảo mật và thanh toán
+                        </a>
+                        <a href="#" class="footer__text">
+                            Chính sách bảo mật thông tin cá nhân
+                        </a>
+                        <a href="#" class="footer__text">
+                            Điều khoản sử dụng
+                        </a>
+                        <a href="#" class="footer__text">
+                            Bán hàng doanh nghiệp
+                        </a>
+
+                    </div>
+                    <div class="col-nho">
+                        <div class="footer__title">
+                            Hợp tác và liên kết
+                        </div>
+                        <a href="#" class="footer__text">
+                            Quy chế hoạt động Sàn GDTMDT
+                        </a>
+                        <a href="#" class="footer__text">
+                            Bán hàng doanh nghiệp
+                        </a>
+                    </div>
+                    <div class="col-nho">
+                        <div class="footer__title">
+                            Phương thức thanh toán
+                        </div>
+                        <div class="footer__img">
+                            <img src="./app/img/visa.svg" alt="">
+                            <img src="./app/img/jcb.svg" alt="">
+                            <img src="./app/img/mastercard.svg" alt="">
+                            <br>
+                            <img src="./app/img/internet-banking.svg" alt="">
+                            <img src="./app/img/cash.svg" alt="">
+                            <img src="./app/img/installment.svg" alt="">
+                        </div>
+                    </div>
+                    <div class="col-nho">
+                        <div class="footer__title">
+                            Kết nối với chúng tôi
+                        </div>
+
+
+                        <div class="footer__title">
+                            Tải ứng dụng
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- End Footer -->
