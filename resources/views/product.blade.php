2177325d4a872db0d56b097a4242d6de73cafce4
LiemLT <leliem28@gmail.com>
2019-12-03 22:55:35 +0700

    Allocate some files.

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,24 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css">
-    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
-    <link rel="stylesheet" href="src/product-styles.css">
-    <title>Product</title>
-</head>
-
-<body>
-    <!-- Header -->
-
-    <!-- End: Header -->
-
-    <!-- Product Information -->
-    <div class="container-fluid">
+@extends('layout.master')
+@section('content')
+<!-- Product Information -->
+<div class="container-fluid product">
         <div class="row">
             <div class="col-lg-5">
                 <div class="carousel">
@@ -93,17 +76,4 @@
         </div>
     </div>
     <!-- End: Product Information -->
-
-    <!-- Footer -->
-
-    <!-- End: Footer -->
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-    <script src="https://kit.fontawesome.com/67aa126bbd.js" crossorigin="anonymous"></script>
-    <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
-    <script type="text/javascript" src="src/js/product.js"></script>
-</body>
-
-</html>
+@endsection
