e84d036bee0b92641b835e2447fa291beaa4907d
Kevin <dqtkien@gmail.com>
2019-12-07 14:29:52 +0700

    add carousel img

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,89 +1,75 @@
-    <!-- Footer -->
-    <footer>
+<!-- Footer -->
+<footer>
         <div class="footer">
             <div class="container">
                 <div class="row">
                     <div class="col-to">
                         <div class="footer__title">
-                            hỗ trợ khách hàng
+                            customer services
                         </div>
                         <div class="footer__text">
                             <div class="footer__emphasis">
-                                Hotline đặt hàng: 1800-6963 <br>
+                                Hotline for booking: 1800-6963 <br>
                             </div>
-                            (Miễn phí , 8-21h kể cả T7, CN) <br>
+                            (Free, 8-21h) <br>
                         </div>
                         <div class="footer__text">
                             <div class="footer__emphasis">
-                                Hotline chăm sóc khách hàng: 1900-6035 <br>
+                                Hotline for services: 1900-6035 <br>
                             </div>
-                            (1000đ/phút , 8-21h kể cả T7, CN)
+                            (0.05$/min, 8-21h)
                         </div>
-                        <a href="#" class="footer__text">Các câu hỏi thưởng gặp</a>
-                        <a href="#" class="footer__text">Các câu hỏi thưởng gặp</a>
-                        <a href="#" class="footer__text">Các câu hỏi thưởng gặp</a>
-                        <a href="#" class="footer__text">Các câu hỏi thưởng gặp</a>
-                        <a href="#" class="footer__text">Các câu hỏi thưởng gặp</a>
-                        <a href="#" class="footer__text">Các câu hỏi thưởng gặp</a>
                     </div>
                     <div class="col-nho">
                         <div class="footer__title">
-                            Về chúng tôi
+                            About US
                         </div>
                         <a href="#" class="footer__text">
-                            Giới thiệu
-                        </a>
-                        <a href="#" class="footer__text">
-                            Chính sách bảo mật và thanh toán
-                        </a>
-                        <a href="#" class="footer__text">
-                            Chính sách bảo mật thông tin cá nhân
-                        </a>
-                        <a href="#" class="footer__text">
-                            Điều khoản sử dụng
-                        </a>
-                        <a href="#" class="footer__text">
-                            Bán hàng doanh nghiệp
-                        </a>
-
-                    </div>
-                    <div class="col-nho">
-                        <div class="footer__title">
-                            Hợp tác và liên kết
-                        </div>
-                        <a href="#" class="footer__text">
-                            Quy chế hoạt động Sàn GDTMDT
-                        </a>
-                        <a href="#" class="footer__text">
-                            Bán hàng doanh nghiệp
+                            WORKING DAYS/HOURS: <br>
+                            Mon - Sun / 9:00 AM - 8:00 PM
                         </a>
                     </div>
                     <div class="col-nho">
                         <div class="footer__title">
-                            Phương thức thanh toán
+                            Connect with us
+                        </div>
+                        <!-- <a href="#" class="footer__text">
+                            ADDRESS: <br>
+                            1234 Street Name, City, England <br>
+                            EMAIL: <br>
+                            mail@example.com
+                        </a> -->
+                        <div class="footer__img" style="font-size: 2rem;">
+                            <a href="#" style="color: #333"><i class="fab fa-facebook-square"></i></a>
+                            <a href="#" style="color: #333"><i class="fab fa-twitter-square"></i></a>
+                            <a href="#" style="color: #333"><i class="fab fa-instagram"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-nho">
+                        <div class="footer__title">
+                            Payment Method
                         </div>
                         <div class="footer__img">
-                            <img src="{{URL::asset('img/visa.svg')}}" alt="">
-                            <img src="{{URL::asset('img/jcb.svg')}}"alt="">
-                            <img src="{{URL::asset('img/mastercard.svg')}}" alt="">
-                            <br>
-                            <img src="{{URL::asset('img/internet-banking.svg')}}"alt="">
-                            <img src="{{URL::asset('img/cash.svg')}}" alt="">
-                            <img src="{{URL::asset('img/installment.svg')}}" alt="">
+                        <img src="{{URL::asset('img/visa.svg')}}" alt="">
+                        <img src="{{URL::asset('img/jcb.svg')}}" alt="">
+                        <br>
+                        <img src="{{URL::asset('img/mastercard.svg')}}" alt="">
+                        <img src="{{URL::asset('img/internet-banking.svg')}}" alt="">
                         </div>
                     </div>
                     <div class="col-nho">
                         <div class="footer__title">
-                            Kết nối với chúng tôi
+                            Contact Us
                         </div>
-
-
-                        <div class="footer__title">
-                            Tải ứng dụng
-                        </div>
+                        <a href="#" class="footer__text">
+                            ADDRESS: <br>
+                            1234 Street Name, City, England <br>
+                            EMAIL: <br>
+                            mail@example.com
+                        </a>
                     </div>
                 </div>
             </div>
         </div>
     </footer>
-    <!-- End Footer -->
+<!-- End Footer -->
--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
@@ -10,8 +10,9 @@
     <link href="https://fonts.googleapis.com/css?family=Oswald:400,700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,500&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/5336ef90a8.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="{{asset('css/main.css')}}">
-    <title>Title</title>
+    <title>FANoFAN</title>
 </head>
 
 <body class="homepage">
