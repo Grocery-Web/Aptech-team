c587f92edf0e73f689d5130a10e3b245073e11c4
Thai Son <Vu>
2019-12-08 21:43:33 +0700

    create table user and product using migration

---
+++ app/Product.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    public $timestamps = false;
+    //
+    protected $fillable = [
+        'name', 'description', 'image','price','type'
+    ];
+
+
+
+    public function getPriceAttribute($value){
+        return $value;
+    }
+}
