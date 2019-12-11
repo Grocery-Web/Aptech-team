88c5ae44fabc15da12327a77d9cb4e2951111ed0
kevin <dqtkien@gmail.com>
2019-12-11 21:09:30 +0700

    add login btn controller

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -8,7 +8,7 @@
     <div class="collapse navbar-collapse" id="headerNav">
         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
             <li class="nav-item">
-                <a class="nav-link" href="./index.html">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="./">Home <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="category-menu" data-toggle="dropdown"
@@ -41,6 +41,27 @@
                 </button>
             </div>
         </form>
+          <!-- Login btn -->
+          <div class="nav__login">
+            <a href="{{ route('login') }}"> <i class="material-icons nav__icon" style="color: #212529">
+                    person_outline
+                </i>
+            </a>
+            @if(Auth::check())
+            <form class="form__text">
+                <div class="nav__login--arrow-up"></div>
+                <div class="nav__login__form">
+                    <p>Email: {!! Auth::user()->email !!}</p>
+                    <p>Name: {!! Auth::user()->name !!}</p>
+                </div>
+                <div class="d-flex justify-content-center">
+                    <a href="{{ route('login') }}" class="btn btn-primary ">Log out</a>
+                </div>
+            </form>
+            @endif
+        </div>
+        <!-- End Login btn -->
+        <!-- Shop Cart  -->
         <div class="nav__shopcart">
             <i class="material-icons-outlined nav__icon">shop</i>
             <div class="form__text nav__shopcart--wrapper">
@@ -83,93 +104,8 @@
                 </div>
             </div>
         </div>
-        <div class="nav__login">
-            <a href=""> <i class="material-icons nav__icon" style="color: #212529">
-                person_outline
-            </i>
-        </a>
-             <!-- User Info -->
-            <!-- <div class="nav__userinfo">
-                        <div class="nav__userinfo--arrow-up"></div>
-                        <div class="nav__userinfo--title">
-                            Thông tin tài khoản
-                        </div>
-                        <div class="nav__userinfo--info">
-                            <div class="row">
-                                <div class="col-md-5">
-                                    Tài khoản
-                                </div>
-                                <div class="col-md-7">
-                                    :Tên tài khoản
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-5">
-                                    Ngày tham gia
-                                </div>
-                                <div class="col-md-7">
-                                    :xx/xx/xxxx
-                                </div>
-                            </div>
-                            <a href="#" class="logout">Đăng xuất</a>
-                        </div>
-                    </div> -->
-            <!-- End User Info -->
-        </div>
-    </div>
+        <!-- End Shop Cart -->
 
     </div>
 </nav>
-<!-- Sign up Popup -->
-<div class="popup">
-    <div class="blackscreen">
-    </div>
-    <div class="nav__signup">
-        <div class="closeup">
-        </div>
-        <div class="nav__signup--left">
-            <h2 class="title">
-                Tạo tài khoản
-            </h2>
-            <p>Tạo tài khoản để theo dõi đơn hàng, lưu danh sách sản phẩm yêu thích, nhận nhiều ưu đãi hấp dẫn.</p>
-            <img src="{{URL::asset('img/graphic-map.png')}}" alt="">
-        </div>
-        <div class="nav__signup--right">
-            <form action="">
-                <div class="form__modal">
-                    <label for="full_name">Họ tên</label>
-                    <input type="text" id="full_name" name="full_name" class="form__text" placeholder="Nhập họ và tên">
-                </div>
-                <div class="form__modal">
-                    <label for="phone_number">SĐT</label>
-                    <input type="text" id="phone_number" name="phone_number" class="form__text" placeholder="Nhập SDT">
-                </div>
-                <div class="form__modal">
-                    <label for="full_name">Địa chỉ</label>
-                    <input type="text" id="address" name="address" class="form__text" placeholder="Nhập địa chỉ">
-                </div>
-                <div class="form__modal">
-                    <label for="">Email</label>
-                    <input type="text" id="email" name="email" class="form__text" placeholder="Nhập địa chỉ email">
-                </div>
-                <div class="form__modal">
-                    <label for="">Mật khẩu</label>
-                    <input type="password" id="password" name="password" class="form__text" placeholder="Nhập mật khẩu">
-                </div>
-                <div class="form__modal">
-                    <label for="">Nhập lại Mật khẩu</label>
-                    <input type="password" id="re_password" name="re_password" class="form__text"
-                        placeholder="Nhập lại mật khẩu">
-                </div>
-                <input type="radio" name="gender" value="male" checked> Nam<br>
-                <input type="radio" name="gender" value="female">Nữ<br>
-                <div class="nav__signup--btn">
-                    <input type="submit" value="Tạo tài khoản" class="form__button">
-                </div>
-
-            </form>
-        </div>
-    </div>
-</div>
-<!-- Sign up Popup -->
 <!-- End Navbar -->
