eb9c8b4db751b830fbb67aa1394195ebf45e6102
Thai Son <Vu>
2019-12-17 22:48:44 +0700

    Authorize admin user

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,13 +15,39 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         DB::table('users')->insert([
-            'USERNAME'    =>   'sonvt8',
+            'USERNAME'    =>   'superadmin',
             'PASSWORD'    =>    Hash::make('secret'),
             'NAME'        =>   'Vũ Thái Sơn',
-            'EMAIL'       =>   'sonvt8@viettel.com.vn',
+            'EMAIL'       =>   'superadmin@gmail.com',
             'PHONE'       =>   '0977777606',
             'ADDRESS'     =>   'Hồ Chí Minh',
-            'SEX'         =>   1
+            'SEX'         =>   1,
+            'IS_ADMIN'    =>   1,
+            'LV_USER'     =>   2
+        ]);
+
+        DB::table('users')->insert([
+            'USERNAME'    =>   'admin',
+            'PASSWORD'    =>    Hash::make('secret'),
+            'NAME'        =>   'Đặng Quang Trung Kiên',
+            'EMAIL'       =>   'admin@gmail.com',
+            'PHONE'       =>   '0933444555',
+            'ADDRESS'     =>   'Hồ Chí Minh',
+            'SEX'         =>   1,
+            'IS_ADMIN'    =>   1,
+            'LV_USER'     =>   1
+        ]);
+
+        DB::table('users')->insert([
+            'USERNAME'    =>   'client',
+            'PASSWORD'    =>    Hash::make('secret'),
+            'NAME'        =>   'Lê Thanh Liêm',
+            'EMAIL'       =>   'client@gmail.com',
+            'PHONE'       =>   '0966777888',
+            'ADDRESS'     =>   'Hồ Chí Minh',
+            'SEX'         =>   1,
+            'IS_ADMIN'    =>   0,
+            'LV_USER'     =>   0
         ]);
     }
 }
