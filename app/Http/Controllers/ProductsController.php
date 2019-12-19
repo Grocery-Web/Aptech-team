48a1ee336be71321adb2397c23364ca2a7a2896f
thangtm <thangtm589@gmail.com>
2019-12-19 10:26:03 +0700

    add cart function

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Product;
+use Cart;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 // use App\Cart;
@@ -12,8 +13,34 @@ class ProductsController extends Controller
     public function index(){
 
         $products = Product::all();
-   
+
         return view("mainpage",compact("products"));
-   
+
+    }
+
+    public function addProductToCart(Request $request, $id) {
+        $prevCart = $request->session()->get('cart');
+        $cart = new Cart($prevCart);
+
+        $product = Product::find($id);
+        $cart->addItem($id, $product);
+        $request->session()->put('cart', $cart);
+
+        return redirect()->route("");
+
+    }
+
+    public function showCart() {
+        $cart = Session::get('cart');
+
+        // check cart is not empty
+        if($cart) {
+            return view('', ["cartItems"=> $cart]);
+        // cart is empty
+        } else {
+            echo "Your cart is empty";
+            return redirect()->route("");
+        }
+
     }
 }
