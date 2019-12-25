7c9419b608432d0dad8773c22d191373b10be9d5
Thai Son <Vu>
2019-12-25 11:49:21 +0700

    edit migration product

--- database/migrations/2019_12_08_143347_create_products_table.php
+++ database/migrations/2019_12_08_143347_create_products_table.php
@@ -15,13 +15,18 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
-            $table->timestamps();
             $table->string('name', 100)->unique();
             $table->text('description', 100);
+            $table->decimal('weight', 4, 2);
+            $table->decimal('width', 4, 2);
+            $table->decimal('depth', 4, 2);
+            $table->decimal('height', 4, 2);
+            $table->text('producer', 100)->nullable();
             $table->string('image')->nullable();
             $table->decimal('price', 8, 2);
             $table->string('type');
             $table->integer('quantity');
+            $table->timestamps();
         });
     }
 
