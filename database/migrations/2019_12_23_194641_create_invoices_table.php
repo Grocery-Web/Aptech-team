c28c2385960c84ee51d2e159a3bd12a8ab8dfd20
thangtm <thangtm589@gmail.com>
2019-12-25 19:34:23 +0700

    adjust invoice and invoice detail table migration

--- database/migrations/2019_12_23_194641_create_invoices_table.php
+++ database/migrations/2019_12_23_194641_create_invoices_table.php
@@ -16,20 +16,16 @@ class CreateInvoicesTable extends Migration
         Schema::create('invoices', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->bigInteger('user_id')->unsigned();
-            $table->integer('product_id')->unsigned();
-            $table->integer('product_quantity');
+            $table->integer('total_quantity');
             $table->integer('total_price');
             $table->string('shipping_address', 300);
+            $table->string('status', 100);
             $table->timestamps();
         });
 
         Schema::table('invoices', function($table) {
             $table->foreign('user_id')
                 ->references('id')->on('users')
-                ->onDelete('cascade');
-
-            $table->foreign('product_id')
-                ->references('id')->on('products')
                 ->onDelete('cascade');
         });
     }
