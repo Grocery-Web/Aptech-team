ac4c99ec767cffd1ce74c57f1514c5e4553271e6
Thai Son <Vu>
2019-12-27 23:43:44 +0700

    make constraint between user and product table

--- database/migrations/2019_12_08_143347_create_products_table.php
+++ database/migrations/2019_12_08_143347_create_products_table.php
@@ -15,6 +15,8 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
             $table->string('name', 100)->unique();
             $table->text('description', 100);
             $table->decimal('weight', 10, 2);
