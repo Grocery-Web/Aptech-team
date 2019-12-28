28bb4d06fd19c2e5a01bf51bc9900fdf53f57d78
LiemLT <leliem28@gmail.com>
2019-12-28 13:54:30 +0700

    Redesign customer feedback section.

--- resources/views/productDetail.blade.php
+++ resources/views/productDetail.blade.php
@@ -5,9 +5,9 @@
         <div class="col-lg-5">
             <div class="product-carousel">
                 @foreach ($gallery as $key => $value)
-                    <div>
-                        <img src="{{asset ('storage')}}/product_images/{{$value->photos}}" alt="">
-                    </div>
+                <div>
+                    <img src="{{asset ('storage')}}/product_images/{{$value->photos}}" alt="">
+                </div>
                 @endforeach
             </div>
         </div>
@@ -29,7 +29,7 @@
             </div>
             <div class="clearfix"></div>
             <div class="price">
-                <h4>{{$product['price']}}</h4>
+                <h4>${{$product['price']}}</h4>
             </div>
             <div class="product-summary mt-4">
                 {{$product['description']}}
@@ -56,7 +56,7 @@
                     <button type="button" class="updown" id="minus" onclick="decreaseValue()" value="-">-</button>
                     <input type="number" id="quantity" min="0" step="1" value="0" />
                     <button type="button" class="updown" id="increase" onclick="increaseValue()" value="+">+</button>
-                
+
                     <div class="addtocart ml-3 mr-3">
                         <a id="addtoCART" href="{{ route('addProductToCart', ['id'=>$product['id']]) }}"><button
                                 type="button" class="btn btn-dark"><i class="fas fa-shopping-cart mr-2"></i>ADD TO
@@ -172,36 +172,74 @@
                 </tr>
             </table>
         </div>
-        <div id="menu2" class="tab-pane">
-            <h4 class="mt-3">Create Review</h4>
-            <form>
-                <div class="form-row">
-                    <div class="form-group col-md-6">
-                        <label for="inputFirstname">Name *</label>
-                        <input type="name" class="form-control" required>
+        <div id="menu2" class="tab-pane py-3">
+            <small>Purchased this product already ? Tell us what you think.</small><br>
+            <button type="button" class="btn btn-success btn-lg font-weight-light mt-2" data-toggle="collapse"
+                data-target="#feedbackForm" aria-expanded="false" aria-controls="feedbackForm">Add your
+                review</button>
+            <div class="collapse" id="feedbackForm">
+                <h5 class="mt-4 mb-3">CREATE REVIEW</h5>
+                <form>
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label for="reviewHeading">Add a headline *</label>
+                            <input type="text" class="form-control" id="reviewHeading"
+                                placeholder="What's most important to know?" required>
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label for="Textarea1">Your review *</label>
+                            <textarea class="form-control" id="Textarea1" rows="3"
+                                placeholder="What did you like or dislike?" required></textarea>
+                        </div>
+                    </div>
+                    <button type="submit" class="btn btn-success">Submit</button>
+                </form>
+            </div>
+            <div class="p-3 mt-4 bg-secondary text-white">There are currently <span>1</span> feedback.</div>
+            <div class="customer-feedback row mt-4">
+                <div class="col-md-3 text-center">
+                    <img src="https://img.icons8.com/ios-filled/50/000000/user-male-circle.png" class="rounded-circle">
+                    <h6>Username</h6>
+                </div>
+                <div class="col-md-9">
+                    <h6 class="mb-1">Excellent fan with a single glaring con,for those young at heart</h6>
+                    <img src="https://img.icons8.com/color/25/000000/checked-checkbox.png">
+                    <small class="text-success">Verified Purchaser</small>
+                    <p class="my-2">Let's be realistic: when you buy a fan, you want it to move the air. This beauty
+                        does it in spades! Even on low, it is still pushing quite a bit of air and does so relatively
+                        quietly. I set it up a few feet from my bed thinking it was "enough". Wrong. It's almost too
+                        much, and from me, that's saying something. If I lay with my back to it (like I normally would)
+                        there is enough wind going past my ears that it make a sound - yes, literally the wind rushing
+                        past my ear. And that's on low from 3 feet away! ... Frankly, I love it! I have had too many
+                        little fans that just couldn't live up to the task. But this little jewel does exactly what it
+                        is supposed to do. But be advised, it makes noise. Especially on high. This is a fan! And I am a
+                        fan of this fan!</p>
+                    <a class="text-primary" data-toggle="collapse" href="#replyForm" role="button" aria-expanded="false"
+                        aria-controls="replyForm">Reply</a>
+                    <form class="collapse mt-2" id="replyForm">
+                        <div class="form-row">
+                            <div class="form-group col-md-12">
+                                <textarea class="form-control" id="replyArea" rows="4"
+                                    placeholder="Write your reply here. Maximum words allowed is 1500."
+                                    required></textarea>
+                            </div>
+                        </div>
+                        <button type="submit" class="btn btn-success">Submit</button>
+                    </form>
+                    <div class="row mt-2">
+                        <div class="col-md-1">
+                            <img src="https://img.icons8.com/ios-filled/50/000000/user-male-circle.png"
+                                class="rounded-circle">
+                        </div>
+                        <div class="col-md-11">
+                            <h6>Username</h6>
+                            <p>Great review! Plan to place the order. Thank you so much for your sharing. </p>
+                        </div>
                     </div>
                 </div>
-                <div class="form-row">
-                    <div class="form-group col-md-6">
-                        <label for="inputEmail4">Email *</label>
-                        <input type="email" class="form-control" id="inputEmail4" required>
-                    </div>
-                </div>
-                <div class="form-row">
-                    <div class="form-group col-md-6">
-                        <label for="inputCity">Address</label>
-                        <input type="text" class="form-control" id="inputCity">
-                    </div>
-                </div>
-                <div class="form-row">
-                    <div class="form-group col-md-6">
-                        <label for="Textarea1">Your review *</label>
-                        <textarea class="form-control" id="Textarea1" rows="3"
-                            placeholder="What did you like or dislike?" required></textarea>
-                    </div>
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
+            </div>
         </div>
     </div>
 </div>
