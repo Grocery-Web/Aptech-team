358e9bcd7e961255a6d622fc7c30d8ade41ddcfb
LiemLT <leliem28@gmail.com>
2020-01-03 22:15:23 +0700

    Remove unnecessary elements in productDetail.

--- resources/views/productDetail.blade.php
+++ resources/views/productDetail.blade.php
@@ -14,18 +14,7 @@
         <div class="col-lg-7">
             <h2>{{$product['name']}}</h2>
             <div class="product-rating">
-                <div class="rate mb-2">
-                    <input type="radio" id="star5" name="rate" value="5" />
-                    <label for="star5" title="text">5 stars</label>
-                    <input type="radio" id="star4" name="rate" value="4" />
-                    <label for="star4" title="text">4 stars</label>
-                    <input type="radio" id="star3" name="rate" value="3" />
-                    <label for="star3" title="text">3 stars</label>
-                    <input type="radio" id="star2" name="rate" value="2" />
-                    <label for="star2" title="text">2 stars</label>
-                    <input type="radio" id="star1" name="rate" value="1" />
-                    <label for="star1" title="text">1 star</label>
-                </div>
+                
             </div>
             <div class="clearfix"></div>
             <div class="price">
@@ -34,16 +23,7 @@
             <div class="product-summary mt-4">
                 {{$product['description']}}
             </div>
-            <div class="sku mt-4">SKU: <b><a href="#" class="text-dark">HTF1220B</a></b></div>
             <div class="categories mt-2">CATEGORIES: <b><a href="#" class="text-dark">{{$product['type']}}</a></b></div>
-            <div class="product-color mt-2">COLOR:
-                <a href="#" class="text-decoration-none">
-                    <div class="filter-color color_1 ml-3"></div>
-                </a>
-                <a href="#" class="text-decoration-none">
-                    <div class="filter-color color_2"></div>
-                </a>
-            </div>
             <div class="product-size mt-2">SIZE:
                 <a href="#" class="badge badge-light ml-2">Extra Large</a>
                 <a href="#" class="badge badge-light ml-2">Large</a>
