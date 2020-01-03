ee6fa3cf35ea31d1aeb440332a8a41987eb703bf
thangtm <thangtm589@gmail.com>
2020-01-03 15:52:03 +0700

    add delete review code

--- app/Http/Controllers/ReviewsController.php
+++ app/Http/Controllers/ReviewsController.php
@@ -4,23 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use App\Product;
-use App\Cart;
-use App\User;
 use App\Review;
 
-class CommentsController extends Controller
+class ReviewsController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function __invoke(Request $request)
-    {
-        //
+    public function deleteReview($id) {
+        Review::find($id)->delete();
+        return redirect()->back();
     }
-
 
 }
