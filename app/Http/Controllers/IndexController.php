181b003c2ec2b3778fc78c011b088bb56d4fcf3a
Thai Son <Vu>
2020-01-04 22:54:56 +0700

    Fix auth to view some tabs on navbar

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Category;
+
+class IndexController extends Controller
+{
+    public function aboutUs()
+    {
+        $parentCategories = Category::where('parent_id',NULL)->get();
+        return view('aboutUs', ['parentCategories' => $parentCategories]);
+    }
+
+    public function contactUs()
+    {
+        $parentCategories = Category::where('parent_id',NULL)->get();
+        return view('contactUs', ['parentCategories' => $parentCategories]);
+    }
+
+    public function sitemap()
+    {
+        $parentCategories = Category::where('parent_id',NULL)->get();
+        return view('sitemap', ['parentCategories' => $parentCategories]);
+    }
+    // Search Products
+    public function search(Request $request){
+        $query = $request->search;
+        $products = Product::where('name','like','%'.$query.'%')
+                            ->orWhere('price',$query)
+                            ->paginate(6);
+        $parentCategories = Category::where('parent_id',NULL)->get();
+
+        return view("searchProduct", ['products' => $products, 'parentCategories' => $parentCategories]);
+    }
+}
