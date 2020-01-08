fa6402e0b4b94e808f563c5f2a7aeb4e230852a1
Thai Son <Vu>
2020-01-08 22:55:44 +0700

    Display users on admin panel without role_id = 1

--- resources/views/user/displayAccounts.blade.php
+++ resources/views/user/displayAccounts.blade.php
@@ -22,7 +22,7 @@
         </thead>
         <tbody>
 
-        @foreach($users as $user)
+        @foreach($users as $key=>$user)
         <tr>
             <td>{{$user['id']}}</td>
             <td><img src="{{asset ('storage')}}/user_images/{{$user['avatar']}}" alt="{{asset ('storage')}}/product_images/{{$user['avatar']}}" width="100" height="100" style="max-height:220px" ></td>
