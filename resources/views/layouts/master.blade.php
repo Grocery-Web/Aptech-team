60add51dc7b351f0aec344483e039e7aafc9b51f
Thai Son <Vu>
2020-01-10 09:06:54 +0700

    sweetalert to oop-up success message

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -36,6 +36,7 @@
     <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
     <script src="{{URL::asset('js/main.js')}}"></script>
     <script src="{{URL::asset('js/product.js')}}"></script>
+    @include('sweetalert::alert')
     <script>
         $('.delete').click(function() {
             var productId = $(this).attr('product_id');
