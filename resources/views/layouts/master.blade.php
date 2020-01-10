c1b21eb972cbd50ea40bc3d1b9bd4741aaa47746
kevin <dqtkien@gmail.com>
2020-01-10 09:29:10 +0700

    refix mainblade v1

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,13 +5,13 @@
 <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Oswald:400,700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,500&display=swap" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
-    <script src="https://kit.fontawesome.com/5336ef90a8.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="{{asset('css/slick.css')}}" />
+    <script src="{{asset('css/font.js')}}"></script>
     <link rel="stylesheet" href="{{asset('css/main.css')}}">
     <link rel="stylesheet" href="{{asset('css/product.css')}}" />
     <link rel="stylesheet" href="{{asset('css/aboutus.css')}}" />
